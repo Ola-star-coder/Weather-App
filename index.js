cc9e0ebde40830711beb5fca889d9c86446b0071
olamide <olamideashaolu318.gmail.com>
2025-05-31 08:53:05 +0100

    commit new codes

--- index.js
+++ index.js
@@ -31,6 +31,71 @@ let weather = {
     }
 };
 
+let geocode = {
+   reverseGeocode: function(latitude, longitude){
+      var api_key = '6371594750c14167878c1657ca04926a';
+
+      // reverse geocoding example (coordinates to address)
+      var query = latitude + ',' + longitude;
+    
+      // forward geocoding example (address to coordinate)
+      // var query = 'Pappelallee 78/79, 10437 Berlin, Germany';
+      // note: query needs to be URI encoded (see below)
+    
+      var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+    
+      var request_url = api_url
+        + '?'
+        + 'key=' + api_key
+        + '&q=' + encodeURIComponent(query)
+        + '&pretty=1'
+        + '&no_annotations=1';
+    
+      // see full list of required and optional parameters:
+      // https://opencagedata.com/api#required-params
+    
+      var request = new XMLHttpRequest();
+      request.open('GET', request_url, true);
+    
+      request.onload = function() {
+        // see full list of possible response codes:
+        // https://opencagedata.com/api#codes
+    
+        if (request.status === 200){
+          // Success!
+          var data = JSON.parse(request.responseText);
+          weather.fetchWeather(data.results[0].components.state);
+        } else if (request.status <= 500){
+          // We reached our target server, but it returned an error
+    
+          console.log("unable to geocode! Response code: " + request.status);
+          var data = JSON.parse(request.responseText);
+          console.log('error msg: ' + data.status.message);
+        } else {
+          console.log("server error");
+        }
+      };
+    
+      request.onerror = function() {
+        // There was a connection error of some sort
+        console.log("unable to connect to server");
+      };
+    
+      request.send();  // make the request
+   },
+   getLocation: function(){
+      function success(data){
+         geocode.reverseGeocode(data.coords.latitude, data.coords.longitude)
+      }
+      if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(success, console.error);
+      }
+      else{
+         weather.fetchWeather("Lagos");
+      }
+   }
+}
+
 document.querySelector(".search button").addEventListener("click", function(){
  weather.search();
 })
@@ -41,4 +106,5 @@ document.querySelector(".search-bar").addEventListener("keypress", function(even
     weather.search();
    }
 })
-weather.fetchWeather("Lagos");
+
+geocode.getLocation();
